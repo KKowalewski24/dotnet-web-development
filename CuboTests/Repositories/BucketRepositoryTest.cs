2877943faae1ba3827d4bc77aaf2ec839673bb28
Kamil Kowalewski <szamil24@gmail.com>
2021-01-07 12:53:02 +0100

    Add unit tests

---
+++ CuboTests/Repositories/BucketRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using CuboCore.Domain;
+using CuboCore.Repositories.Buckets;
+using Xunit;
+
+namespace CuboTests.Repositories {
+
+    public class BucketRepositoryTest {
+
+        /*------------------------ FIELDS REGION ------------------------*/
+
+        /*------------------------ METHODS REGION ------------------------*/
+        [Fact]
+        public async Task CreateNewBucket() {
+            IBucketRepository bucketRepository = new InMemoryBucketRepository();
+            const string bucketName = "First";
+            Bucket bucket = new Bucket(bucketName);
+
+            await bucketRepository.AddAsync(bucket);
+            Bucket fetchedBucket = await bucketRepository.GetAsync(bucketName);
+
+            Assert.Same(bucket, fetchedBucket);
+        }
+
+    }
+
+}
