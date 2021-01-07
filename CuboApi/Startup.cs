d7356a848e2232b3497d7b9c6b8930a654b9d299
Kamil Kowalewski <szamil24@gmail.com>
2021-01-07 21:13:01 +0100

    Add DataInitializer

--- CuboApi/Startup.cs
+++ CuboApi/Startup.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CuboCore.Mappers;
 using CuboCore.Repositories.Buckets;
+using CuboCore.Services;
 using CuboCore.Services.Buckets;
 using CuboCore.Services.Items;
 using Microsoft.AspNetCore.Builder;
@@ -26,6 +27,7 @@ namespace CuboApi {
             services.AddSingleton<IBucketRepository, InMemoryBucketRepository>();
             services.AddScoped<IBucketService, BucketService>();
             services.AddScoped<IItemService, ItemService>();
+            services.AddSingleton<IDataInitializer, DataInitializer>();
             services.AddSingleton<IMapper>(_ => AutoMapperConfig.InitializeMapper());
             services.AddControllers();
         }
@@ -43,6 +45,11 @@ namespace CuboApi {
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            IDataInitializer initializer = app.ApplicationServices.GetService<IDataInitializer>();
+            if (initializer != null) {
+                initializer.SeedAsync(15);
+            }
         }
 
     }
