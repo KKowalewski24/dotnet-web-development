3e6fa2ba7ad7a52dcc0dd9d559e62a203cb31d68
Kamil Kowalewski <szamil24@gmail.com>
2021-01-07 20:46:57 +0100

    Add more in BucketControllerTests

--- CuboApiTests/Controllers/BucketControllerTests.cs
+++ CuboApiTests/Controllers/BucketControllerTests.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
 using System.Threading.Tasks;
 using CuboApi;
+using CuboApi.Constants;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
@@ -12,6 +16,14 @@ namespace CuboApiTests.Controllers {
     public class BucketControllerTests {
 
         /*------------------------ FIELDS REGION ------------------------*/
+        private const string PATH_BUCKET_CONTROLLER =
+            Constants.API + Constants.SLASH + Constants.BUCKET;
+
+        private const string PATH_PARAM_BUCKET_NAME =
+            Constants.SLASH + Constants.PARAM_BUCKET_NAME;
+
+        private const string BucketName = "First";
+
         private readonly TestServer _testServer;
         private readonly HttpClient _httpClient;
 
@@ -23,7 +35,7 @@ namespace CuboApiTests.Controllers {
 
         [Fact]
         public async Task GetAsync_ReturnEmptyCollection() {
-            HttpResponseMessage response = await _httpClient.GetAsync("api/bucket");
+            HttpResponseMessage response = await _httpClient.GetAsync(PATH_BUCKET_CONTROLLER);
             response.EnsureSuccessStatusCode();
 
             string content = await response.Content.ReadAsStringAsync();
@@ -31,6 +43,29 @@ namespace CuboApiTests.Controllers {
                 JsonConvert.DeserializeObject<IEnumerable<string>>(content);
 
             Assert.Empty(buckets);
+        }
+
+        [Fact]
+        public async Task CreateBucket() {
+            HttpResponseMessage response = await _httpClient.PostAsync(
+                PATH_BUCKET_CONTROLLER + PATH_PARAM_BUCKET_NAME,
+                GetPayload(new { })
+            );
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(response.StatusCode, HttpStatusCode.Created);
+            Assert.Equal(
+                response.Headers.Location.ToString(),
+                Constants.BUCKET + "s" + PATH_PARAM_BUCKET_NAME
+            );
+        }
+
+        private StringContent GetPayload(object data) {
+            return new StringContent(
+                JsonConvert.SerializeObject(data),
+                Encoding.UTF8,
+                MediaTypeNames.Application.Json
+            );
         }
 
     }
